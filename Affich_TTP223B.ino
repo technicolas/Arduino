6c971b9500e77294fc6e9a2c33faf95a67f09952
Nicolas <technicolas@gmail.com>
2021-02-13 01:03:36 +0100

    Add files via upload

--- Affich_TTP223B.ino
+++ Affich_TTP223B.ino
@@ -3,11 +3,13 @@
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-int ledPin = 13;                          // Pin pour la  LED
+int ledV = 13;                            // Pin pour la LED Verte
+int ledR = 7;                            // Pin pour la LED Rouge
  
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);  
+  pinMode(ledV, OUTPUT);
+  pinMode(ledR, OUTPUT);
   pinMode(captTouch, INPUT);
   lcd.init();
 }
@@ -19,26 +21,28 @@ void loop() {
   int ctsValue = digitalRead(captTouch);
   
   if (ctsValue == HIGH){
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledV, HIGH);
+    digitalWrite(ledR, LOW);
     //Serial.println("Capteur touché");
     lcd.setBacklight(1);                  // Active le rétro-éclairage
-    lcd.setCursor(0,0);                  // Se positionne au caractère 0 de la première ligne 0
-    lcd.print("PORTE OUVERTE   ");           // Afficher texte ligne 0
+    lcd.setCursor(0,0);                   // Se positionne au caractère 0 de la première ligne 0
+    lcd.print("PORTE OUVERTE   ");        // Afficher texte ligne 0
     lcd.setCursor(0,1);                   // Se positionne au caractère 0 de la première ligne 1
-    lcd.print("                ");           // Afficher texte ligne 1
-    for(int i=5;i>0;i--){
-      lcd.setCursor(6,1);
+    lcd.print("                ");        // Afficher texte ligne 1
+    for(int i=0;i<6;i++){
+      lcd.setCursor(i,1);
       lcd.print(i);                       // Afficher texte ligne 1
       delay(1000);
     }
-    lcd.setCursor(0,0);                  // Se positionne au caractère 0 de la première ligne 0
-    lcd.print("FERMETURE PORTE ");           // Afficher texte ligne 0
+    lcd.setCursor(0,0);                   // Se positionne au caractère 0 de la première ligne 0
+    lcd.print("FERMETURE PORTE ");        // Afficher texte ligne 0
     lcd.setCursor(0,1);                   // Se positionne au caractère 0 de la première ligne 1
-    lcd.print("    en cours    ");           // Afficher texte ligne 1
+    lcd.print("    en cours    ");        // Afficher texte ligne 1
     delay(2000);
   }
   else{
-    digitalWrite(ledPin,LOW);
+    digitalWrite(ledV,LOW);
+    digitalWrite(ledR, HIGH);
     //Serial.println("Capteur non touché");
     lcd.setBacklight(0);
     lcd.setCursor(0,0);
