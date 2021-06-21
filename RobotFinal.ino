987c09f0cdb842b723c323008b13a55b2666cb8c
Nicolas <technicolas@gmail.com>
2021-06-21 17:59:57 +0200

    Add files via upload

--- RobotFinal.ino
+++ RobotFinal.ino
@@ -1,19 +1,32 @@
-const byte sortie[] = {
-  2,  // moteur gauche pin1 = 2;
-  3,  // moteur gauche pin2 = 3;
-  4,  // moteur droit  pin1 = 4;
-  5,  // moteur droit  pin2 = 5;
-  6,  // Contrôle la LED qui simule le moteur GAUCHE.
-  7,  // Contrôle la LED qui simule le moteur GAUCHE et DROIT.
-  8,  // Contrôle la LED qui simule le moteur DROIT.
+const byte entree[] = {
+  9,            // bouton debug = 9;
   };
+
+const byte sortie[] = {
+  2,            // moteur gauche pin1 = 2;
+  3,            // moteur gauche pin2 = 3;
+  4,            // moteur droit  pin1 = 4;
+  5,            // moteur droit  pin2 = 5;
+  6,            // Contrôle la LED qui simule le moteur GAUCHE.
+  7,            // Contrôle la LED qui simule le moteur GAUCHE et DROIT.
+  8,            // Contrôle la LED qui simule le moteur DROIT.
+  LED_BUILTIN,  // Contrôle la LED interne.
+  };
+
+#define SEUIL 700
+bool debug = false;
 
 void setup() {
   Serial.begin(9600);
-  for(int i=0; i<7; i++){
+
+  for(int i=0; i<1; i++){
+    pinMode(entree[i], INPUT_PULLUP);
+  }
+  
+  for(int i=0; i<8; i++){
     pinMode(sortie[i], OUTPUT);
   }
-  roueGplusD();                                         // Au démarrage, on place la voiture sur la ligne noire et face à la photorésitance centrale.
+  stop();                                               // Au démarrage, on place la voiture sur la ligne noire et face à la photorésitance centrale.
 }
 
 void loop() {
@@ -21,14 +34,12 @@ void loop() {
   int Capt_M = analogRead(A1);                          // Valeur provenant de la photorésistance située au milieu.
   int Capt_D = analogRead(A2);                          // Valeur provenant de la photorésistance située à droite.
   
+  bool debug = etatDebug();
   testLigne(Capt_G, Capt_M, Capt_D);
-//  console(false, Capt_G, Capt_M, Capt_D);
+  
+  if(debug)
+    console();
 }
-
-
-// TODO Intégrer un mode DEBUG
-// if(!debug) then...
-
 
 
 // --------------------------------------------------
@@ -36,35 +47,30 @@ void loop() {
 // --------------------------------------------------
 
 void avant() {
-  digitalWrite(sortie[0], HIGH);
-  digitalWrite(sortie[1], LOW);
-  digitalWrite(sortie[2], HIGH);
-  digitalWrite(sortie[3], LOW);
+  digitalWrite(sortie[0], LOW);
+  digitalWrite(sortie[1], HIGH);
+  digitalWrite(sortie[2], LOW);
+  digitalWrite(sortie[3], HIGH);
 }
 
 void gauche() {
-  digitalWrite(sortie[0], LOW);
-  digitalWrite(sortie[1], LOW);
-  digitalWrite(sortie[2], HIGH);
-  digitalWrite(sortie[3], LOW);
+  for(int i=0; i<3; i++){
+    digitalWrite(sortie[i], LOW);
+  }
+  digitalWrite(sortie[3], HIGH);
 }
 
 void droite() {
-  digitalWrite(sortie[0], HIGH);
-  digitalWrite(sortie[1], LOW);
+  digitalWrite(sortie[0], LOW);
+  digitalWrite(sortie[1], HIGH);
   digitalWrite(sortie[2], LOW);
   digitalWrite(sortie[3], LOW);
 }
 
 void arret() {
-  digitalWrite(sortie[0], LOW);
-  digitalWrite(sortie[1], LOW);
-  digitalWrite(sortie[2], LOW);
-  digitalWrite(sortie[3], LOW);
-}
-
-void repos(int temps){
-  delay(temps);
+  for(int i=0; i<4; i++){
+    digitalWrite(sortie[i], LOW);
+  }
 }
 
 void testLigne(int Capt_G, int Capt_M, int Capt_D){
@@ -74,38 +80,65 @@ void testLigne(int Capt_G, int Capt_M, int Capt_D){
     // Si la voiture va trop à gauche, le capteur droit sera dans le noir; il faudra alors demander à la roue gauche d'accélérer ou à la roue droite de ralentir un peu.
     // Par contre, si le capteur du milieu est sur la bande noire, on demande aux deux roues d'avancer normalement.
     
-  if(Capt_G < 600) roueG();                             // Ligne noire devant la photorésistance de gauche, mise en fonction du moteur gauche.
-  if(Capt_M < 600) roueGplusD();                        // Ligne noire devant la photorésistance centrale, mise en fonction du moteur gauche PLUS droit.
-  if(Capt_D < 600) roueD();                             // Ligne noire devant la photorésistance de droite, mise en fonction du moteur droit.
+  if(Capt_G < SEUIL) 
+    roueD();                             // Ligne noire devant la photorésistance de gauche, mise en fonction du moteur droite.
+  if(Capt_M < SEUIL)
+    roueGplusD();                        // Ligne noire devant la photorésistance centrale, mise en fonction du moteur gauche PLUS droit.
+  if(Capt_D < SEUIL)
+    roueG();                             // Ligne noire devant la photorésistance de droite, mise en fonction du moteur gauche.
+  if(Capt_G > SEUIL && Capt_M > SEUIL && Capt_D > SEUIL)
+    arret();
   delay(100);
 }
 
 void roueG(){
-  digitalWrite(2, HIGH);                                // LED GAUCHE allumée, les autres éteintes.
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
+  digitalWrite(sortie[4], HIGH);                                // LED GAUCHE allumée, les autres éteintes.
+  digitalWrite(sortie[5], LOW);
+  digitalWrite(sortie[6], LOW);
+  
   gauche();
 }
 
 void roueGplusD(){
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);                                // LED CENTRALE allumée, les autres éteintes.
-  digitalWrite(4, LOW);
+  digitalWrite(sortie[4], LOW);
+  digitalWrite(sortie[5], HIGH);                                // LED CENTRALE allumée, les autres éteintes.
+  digitalWrite(sortie[6], LOW);
+  
   avant();
 }
 
 void roueD(){
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, HIGH);                                // LED DROITE allumée, les autres éteintes.
+  digitalWrite(sortie[4], LOW);
+  digitalWrite(sortie[5], LOW);
+  digitalWrite(sortie[6], HIGH);                                // LED DROITE allumée, les autres éteintes.
+  
   droite();
 }
 
-void console(bool etat, int Capt_G, int Capt_M, int Capt_D){
-  if (etat==1){
-    Serial.print("Capteur Gauche: "); Serial.println(Capt_G);
-    Serial.print("Capteur Milieu: "); Serial.println(Capt_M);
-    Serial.print("Capteur Droit: "); Serial.println(Capt_D);
-    delay(1000);
+void stop(){
+  digitalWrite(sortie[4], LOW);
+  digitalWrite(sortie[5], LOW);
+  digitalWrite(sortie[6], LOW);                                // LED DROITE allumée, les autres éteintes.
+  
+  arret();
+}
+
+void console(){
+  Serial.print("Capteur Gauche: "); Serial.println(analogRead(A0));
+  Serial.print("Capteur Milieu: "); Serial.println(analogRead(A1));
+  Serial.print("Capteur Droit: "); Serial.println(analogRead(A2));
+}
+
+bool etatDebug(){
+  bool etat;
+  int etatBouton = digitalRead(entree[0]);
+
+  if(etatBouton == HIGH){
+    digitalWrite(LED_BUILTIN, LOW);
+    return etat;
+  }
+  else{
+    digitalWrite(LED_BUILTIN, HIGH);
+    return etat;
   }
 }
