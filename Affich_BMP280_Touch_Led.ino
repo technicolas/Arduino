63daca29e2aee297a38271a8003d5057b288a01a
Nicolas <technicolas@gmail.com>
2022-07-24 18:21:20 +0200

    Add files via upload

--- Affich_BMP280_Touch_Led.ino
+++ Affich_BMP280_Touch_Led.ino
@@ -15,7 +15,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 int lastButtonState;
 int currentButtonState;
-int temperature[]={0,10,15,20,21,22,25,30,35,40,45};
+int temperature[]={0,10,15,20,21,22,25,30,35,40,45}; // Températures de consigne
 int temp = 5;
 
 unsigned long delayTime;
@@ -36,38 +36,39 @@ void loop() {
   effaceEcran();
   afficheur();
   alarmeTemp();
-  if(digitalRead(CAPTEUR_TOUCH) == HIGH)
-    temp++;
-  if (temp>12)
-    temp=0;
+  if(digitalRead(CAPTEUR_TOUCH) == HIGH)    // Si appui sur le capteur touch...
+    temp++;                                 // ... on incrémente
+    
+  if (temp>12)                              // Si on arrive au bout du tableau...
+    temp=0;                                 // ... on revient au début du tableau
   printConsole();
   delay(delayTime);
 }
 
 void afficheur() {
-  lcd.setBacklight(1);                  // Active le rétro-éclairage
+  lcd.setBacklight(1);                    // Active le rétro-éclairage
   
   // Ligne du HAUT:
   lcd.setCursor(0,0);
-  lcd.print(bme.readTemperature());
+  lcd.print(bme.readTemperature());       // Affichage de la température instantanée
   lcd.setCursor(5,0);
   lcd.print((char)223);
   lcd.setCursor(7,0);
   lcd.print("Hum");
   lcd.setCursor(10,0);
-  lcd.print(bme.readHumidity());
+  lcd.print(bme.readHumidity());          // Affichage de l'humidité
   lcd.setCursor(15,0);
   lcd.print("%");
 
   // Ligne du BAS:
   lcd.setCursor(0,1);
-  lcd.print(bme.readPressure() / 100.0F);
+  lcd.print(bme.readPressure() / 100.0F); // Affichage de la pression
   lcd.setCursor(7,1);
   lcd.print("hPA");
   lcd.setCursor(12,1);
   lcd.print("C:");
   lcd.setCursor(14,1);
-  lcd.print(temperature[temp]);
+  lcd.print(temperature[temp]);           // Affichage de la température de consigne
 }
 
 void effaceEcran(){
@@ -78,15 +79,15 @@ void effaceEcran(){
 }
 
 void alarmeTemp() {
-  if(bme.readTemperature()>temperature[temp]){
-    digitalWrite(LEDR, HIGH);
+  if(bme.readTemperature()>temperature[temp]){    // Si dépassement de la consigne...
+    digitalWrite(LEDR, HIGH);                       // ... on allume la LED
     //    tone(BUZZER, 1000);   // Send 1KHz sound signal...
     //    delay(100);           // 1000...for 1 sec
     //    noTone(BUZZER);       // Stop sound...
     //    delay(100);           // 1000...for 1 sec
   }
-  else
-    digitalWrite(LEDR, LOW);
+  else                                              // Sinon...
+    digitalWrite(LEDR, LOW);                        // ... on éteint la LED
 }
 
 void printConsole() {
